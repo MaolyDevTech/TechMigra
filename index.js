2454afae6a80b90f89d34a06f748f8439320ec5d
MaolyDevTech <maolydevtech@gmail.com>
2023-09-10 20:20:46 -0300

    refactor: add rota soma

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 
-app.get('/home', (req, res) => {
-  res.send('Hello World')
-})
+// Rota
+// Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+//GET - Pega um info
+//POST - Cria uma info
+//PUT - Altera toda a info
+//PATH - Altera parte da info
+//DELETE - Apaga a Info
+// Name - Um indentificador da rota
+//Function (Callback) - Responsavel por executar algum comando
+
+
+app.get('/soma', (req, res) => {
+  const soma = 100 + 1;
+
+  res.send({ soma: soma })
+});
 
 app.listen(3000)
