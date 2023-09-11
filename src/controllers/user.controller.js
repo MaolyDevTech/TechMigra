a17cc8a0183f4a793dea47e5f970a5ff21f9483d
MaolyDevTech <maolydevtech@gmail.com>
2023-09-11 00:31:03 -0300

    Refactor: add rota findAll e findById

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userService = require('../services/user.service');
+const mongoose = require ('mongoose');
 
 const create = async (req, res) => {
     const { name, username, email, password, avatar, background } = req.body;
@@ -7,7 +8,7 @@ const create = async (req, res) => {
         res.status(400).send({ message: "Submit all fields for registration" })
     }
 
-    const user = await userService.create(req.body);
+    const user = await userService.createService(req.body);
 
     if (!user) {
         return res.status(400).send({ message: "Error creating User" });
@@ -26,4 +27,37 @@ const create = async (req, res) => {
     });
 };
 
-module.exports = { create };
+const findAll = async (req, res) => {
+    const users = await userService.findAllService();
+
+    if (!users.length === 0) {
+        return res.status(400).send({ message: "There are not registered users" });
+    }
+
+    res.status(200).send({
+        message: "Users retrieved successfully",
+        users,
+    });
+
+}
+
+const findById = async (req, res) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid id" });
+    }
+
+    const user = await userService.findByIdService(id);
+
+    if (!user) {
+        return res.status(400).send({ message: "User not found" });
+    }
+
+    res.status(200).send({
+        message: "User retrieved successfully",
+        user,
+    });
+}
+
+module.exports = { create, findAll, findById };
